bc0e68742bd8b367f615f7e2b74ed1a23eca55d9
Willian Paulon <willianpaulon50@gmail.com>
2023-07-28 21:02:11 -0300

    .

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ const song = document.getElementById('audio');
 const play = document.getElementById('play');
 
 songName.innerText = 'Through the Valley';
+let isPlaying = false;
 
 function playSong(){
-
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     song.play();
+    isPlaying = true;
 
 }
+
+function pauseSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+
+}
+
+function playPauseDecider(){
+
+    if(isPlaying === true) {
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+
+
+}
+
+
+
 
 play.addEventListener('click', playSong);
